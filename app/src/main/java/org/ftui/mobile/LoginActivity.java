6d4522fd075e385b818f7d34a665fdec85e7a50f
Farhan Hanif <madjavacoder@gmail.com>
2019-07-25 22:36:33 +0700

    Login interface is now done, thanks inkscape for saving my sanity

---
+++ app/src/main/java/org/ftui/mobile/LoginActivity.java
@@ -0,0 +1,22 @@
+package org.ftui.mobile;
+
+import android.os.Build;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            getWindow().setStatusBarColor(getResources().getColor(R.color.colorPrimaryDark, this.getTheme()));
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setStatusBarColor(getResources().getColor(R.color.colorPrimaryDark));
+        }
+
+    }
+}
